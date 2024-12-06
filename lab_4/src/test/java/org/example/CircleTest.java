95dea3ae1b96fae7e1f5f5e5e6aa1e831d33d897
Bartek <bfilipak@interia.pl>
2024-12-06 18:14:28 +0100

    w koncu dziala

--- lab_4/src/test/java/org/example/CircleTest.java
+++ lab_4/src/test/java/org/example/CircleTest.java
@@ -12,7 +12,7 @@ public class CircleTest {
     @Test
     public void testGetPerimeter() {
         Circle circle = new Circle("test Circle",7);
-        double expectedPerimeter = Math.PI*7*7;
+        double expectedPerimeter = 2*Math.PI*7;
         assertEquals(expectedPerimeter, circle.getPerimeter(), 0.0001);
     }
     @Test
